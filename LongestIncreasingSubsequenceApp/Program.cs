38ba7d8a7ec93b6a7a34d6441be54d20fe0150ff
Scott Atkinson <scott.atkinson@adrenalinmedia.com.au>
2023-09-17 18:34:40 +1000

    Updated main App to use the new Service layer

--- LongestIncreasingSubsequenceApp/Program.cs
+++ LongestIncreasingSubsequenceApp/Program.cs
@@ -1,18 +1,42 @@
-﻿using static System.Net.Mime.MediaTypeNames;
-
+﻿using LongestIncreasingSubsequenceApp.Services.Interfaces;
+using LongestIncreasingSubsequenceApp.Services.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 internal class Program
 {
     static void Main(string[] args)
     {
-        var input = string.Join(" ", args);
-        var result = GetLongestIncreasingSubsequence(input);
-    }
 
+        // Setup Dependancy Injection
+        var serviceProvider = new ServiceCollection()
+            .AddLogging()
+            .AddSingleton<IIncreasingSubsequenceService, IncreasingSubsequenceService>()
+            .BuildServiceProvider();
 
-    static string GetLongestIncreasingSubsequence(string inputStr)
-    {
+        // Setup Logging
+        var logger = serviceProvider.GetService<ILoggerFactory>().CreateLogger<Program>();
+        
+        // Check we actually have parameters
+        if (args.Length == 0)
+        {
+            logger.LogCritical("No arguments passed to the application.");
+            return;
+        }
 
-        return string.Empty;
+        // Try and pass the string array to an int array.
+        var sequence = string.Join(" ", args).Split(' ').Select(int.Parse).ToArray();
 
+        // Only proceed if we actually have a sequence, otherwise just log a error
+        if (sequence.Length > 0)
+        {
+            var service = serviceProvider.GetService<IIncreasingSubsequenceService>();
+            var result = service?.GetIncreasingSubsequence(sequence);
+            Console.WriteLine(result);
+        }
+        else
+        {
+            logger.LogCritical("Unable to pass inputs to int(s)");
+        }
     }
 }
+    
